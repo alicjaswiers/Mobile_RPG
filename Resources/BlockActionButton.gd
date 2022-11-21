ae0dc355825e41106d8bbef16d714d6785ef2a6f
alicjaswiers <alicjaswiers@wp.pl>
2022-11-21 22:28:53 +0100

    Animations
    Bow, Regen and Heal animations

--- Resources/BlockActionButton.gd
+++ Resources/BlockActionButton.gd
@@ -1,9 +1,20 @@
 extends "res://Resources/ActionButton.gd"
 
+const Regen = preload("res://Resources/Images/Animations/Regen.tscn")
+
 func _on_pressed():
+	var main = get_tree().current_scene
+	var enemy = BattleUnits.Enemy
 	var playerStats = BattleUnits.PlayerStats
 	if playerStats != null:
 		if playerStats.mp >= 1:
 			if playerStats.ap == 3:
+				create_regen(enemy.global_position)
 				playerStats.ap -= 3
 				playerStats.mp += 8
+
+func create_regen(position):
+	var regen = Regen.instance()
+	var main = get_tree().current_scene
+	main.add_child(regen)
+	regen.global_position = position
--- Resources/HealActionButton.gd
+++ Resources/HealActionButton.gd
@@ -1,9 +1,20 @@
 extends "res://Resources/ActionButton.gd"
 
+const Heal = preload("res://Resources/Images/Animations/Heal.tscn")
+
 func _on_pressed():
+	var main = get_tree().current_scene
+	var enemy = BattleUnits.Enemy
 	var playerStats = BattleUnits.PlayerStats
 	if playerStats != null:
 		if playerStats.mp >= 8:
+			create_heal(enemy.global_position)
 			playerStats.hp += 5
 			playerStats.ap -= 1
 			playerStats.mp -= 8
+
+func create_heal(position):
+	var heal = Heal.instance()
+	var main = get_tree().current_scene
+	main.add_child(heal)
+	heal.global_position = position
