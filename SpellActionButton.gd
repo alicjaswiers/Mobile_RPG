74a200981da69a8da501136c761324bed08167f5
alicjaswiers <alicjaswiers@wp.pl>
2022-11-22 23:38:31 +0100

    Spell animation

--- SpellActionButton.gd
+++ SpellActionButton.gd
@@ -1,6 +1,6 @@
 extends "res://Resources/ActionButton.gd"
 
-const Slash = preload("res://Resources/Slash.tscn")
+const Spell = preload("res://Resources/Images/Animations/Spell.tscn")
 
 func _on_pressed():
 	var main = get_tree().current_scene
@@ -8,13 +8,13 @@ func _on_pressed():
 	var playerStats = BattleUnits.PlayerStats
 	if enemy != null and playerStats != null:
 		if playerStats.mp >=6:
-			create_slash(enemy.global_position)
+			create_spell(enemy.global_position)
 			enemy.take_damage(6)
 			playerStats.mp -= 4
 			playerStats.ap -= 1
 		
-func create_slash(position):
-	var slash = Slash.instance()
+func create_spell(position):
+	var spell = Spell.instance()
 	var main = get_tree().current_scene
-	main.add_child(slash)
-	slash.global_position = position
+	main.add_child(spell)
+	spell.global_position = position
